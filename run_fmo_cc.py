eb05fdfc5f11e322ab3c5d1075003a1b33a923db
Anish9435 <anish.saheb41@gmail.com>
2025-08-19 17:21:55 +0530

    [Added] added a new file to run the fmo_cc code

---
+++ run_fmo_cc.py
@@ -0,0 +1,17 @@
+import sys
+from fmocc.fmo_processor import FMOProcessor #type: ignore
+from fmocc.logger import get_logger #type: ignore
+
+def main():
+    logger = get_logger(__name__)
+    try:
+        processor = FMOProcessor("input.json")
+        E_cc, E_cc_tot = processor.run()
+        logger.info(f"CC Correlation Energy: {E_cc:.6f}")
+        logger.info(f"Total CC Energy: {E_cc_tot:.6f}")
+    except Exception as e:
+        logger.error(f"Error running FMO-CC: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()

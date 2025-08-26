629e3859d70254d0a7670633412b51df243c7059
Anish9435 <anish.saheb41@gmail.com>
2025-08-26 16:57:58 +0530

    [Modified] Streamlined the input argument, now custom input is supported

--- src/fmocc/__main__.py
+++ src/fmocc/__main__.py
@@ -1,18 +1,31 @@
 import os
 import sys
+import argparse
 from fmocc.fmo_processor import FMOProcessor
 from fmocc.utils import HelperFunction, FMOCC_LOGGER
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Run FMO-CC calculations with a specified configuration file."
+    )
+    parser.add_argument(
+        "-c", "--config",
+        type=str,
+        default="input.json",
+        help="Path to the input JSON configuration file (default: input.json)."
+    )
+    args = parser.parse_args()
     logger = FMOCC_LOGGER
     try:
-        processor = FMOProcessor("input.json")
+        processor = FMOProcessor(args.config)
         E_cc, E_cc_tot = processor.run()
         logger.info(f"CC Correlation Energy: {E_cc:.9f}")
         logger.info(f"Total CC Energy: {E_cc_tot:.9f}")
+
         fmocc_dir = os.path.dirname(__file__)
         logger.info(f"Clearing __pycache__ in directory: {fmocc_dir}")
         HelperFunction.clear_pycache(fmocc_dir)
+    
     except Exception as e:
         logger.error(f"Error running FMO-CC: {e}")
         sys.exit(1)
